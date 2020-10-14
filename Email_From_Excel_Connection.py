7aaf2af4b4e9a5fe059bf5b528db1bf5ccc9e351
Ken Cavanagh <36281899+kfcav@users.noreply.github.com>
2020-10-14 16:31:16 -0400

    Update Email_From_Excel_Connection.py

--- Email_From_Excel_Connection.py
+++ Email_From_Excel_Connection.py
@@ -1,43 +1,19 @@
 # Refresh the data connection, Load the file, manipulate the dataframe, send an email with the info
 
 # Import Packages
-
-import win32com.client as win32                         # For working with excel files and refreshing the connection
-import pandas as pd                                     # For Data manipulation
-import smtplib                                          # For Sending emails
-from email.mime.text import MIMEText                    # MIME helps us format emails
-from email.mime.application import MIMEApplication      # MIME also allows us to email data in a tabular (or DF) format 
-from email.mime.multipart import MIMEMultipart          
-from datetime import date                               # For adding todays date into the subject line
-
+import Refresh_Excel
+import Email_DF
+import pandas as pd
 
 #PART 1: REFRESH THE DATA CONNECTION IN THE EXCEL WORKBOOK
 
-path = r'C:\Users\username\Desktop\PATH\FileName.xlxs'  # Input your workbook's path here
+file_path = r'C:\Users\username\Desktop\PATH\FileName.xlxs'  # Input your workbook's path here
 
-xl = win32.client.DispatchEx('Excel.Application')       # Start an instance of Excel
-
-wb = xl.workbooks.Open(path)                            # Open the workbook
-
-wb.RefreshAll()                                         # Refresh all data connections in said workbook
-
-xl.CalculateUntilAsyncQueriesDone()                     # Holds the program until the refresh has completed
-
-xl.DisplayAlerts = False                                # Stops the dialog box from popping up
-
-wb.Save()                                               # Saves the file
-
-wb.Close()                                              # Closes the workbook
-xl.Quit()                                               # Quits the connection
-
-#del wb                                                  # Optional - use these when you are scheduling this program to run
-#del xlapp                                               # They terminate Excel in the background (Task Manager) which ensures a proper refresh
-                                                        
-
+refresh_excel(file_path=file_path)                                                        
 
 #PART 2: LOAD IN THE FILE AND TRANSFORM DATA
 
-df = pd.read_excel(path)                                # Read in the file
+df = pd.read_excel(file_path)                           # Read in the file
 
 data = df[['Column_A', 'Column_B']]                     # Create a new dataframe with only the columns we want to report on
                                                         # Replace with your columns
@@ -50,34 +26,5 @@ host = 'smtp.gmail.com'                                  # Smtp connection domai
 email = 'email@domain.com'                               # Email of the associated account which will be used for login credentials
 pw = 'pw'                                                # Password for login credentials
 recipients = ['email@domain.com']                        # Who the email will be sent to. Can be one or multiple people in the list
-#emaillist = [elem.strip().split(',') for elem in recipients]   # If you want to send to multiple recipients, will parse from the above list
 
-msg = MIMEMultipart()                                       # Variable to initiate the MIME function for inputting different pieces of the email
-today = date.today()                                        # Todays date unformatted (for use in the subject)
-d2 = today.strftime("%B %d, %Y")                            # Todays date formatted as textual month, then day and year (e.g. 'July 13, 2020')
-msg['Subject'] = 'Your Data as of ' + str(d2)  # Subject line
-msg['From'] = 'email@domain.com'                            # Who the email is being sent from (probably same as the email variable)
-
-# This bit formats the DF into an html format
-
-html = """\
-<html>
-  <head></head>
-  <body>
-    {0}
-  </body>
-</html>
-""".format(data.to_html())                            # This is what allows the DF to appear as a table in the email
-                                                      # Note the data.to_html method
-
-part1 = MIMEText(html, 'html')                        # Further html formatting
-msg.attach(part1)
-
-# The bit that actually sends the email
-
-con = smtplib.SMTP(host, 587)                          # Connection variable
-con.ehlo()                                             # Starts the connection
-con.starttls()                                         # Starts the encryption
-con.login(email, pw)                                   # Logs you in      
-con.sendmail(msg['From'], recipients , msg.as_string())# Sends your email, change recipients to emaillist if sending to multiple recipients
-con.quit()                                             # Quits the connection
+email_df(data=data, host=host, email=email, pw=pw, recipients=recipients, subject='your_subject' From=email)
